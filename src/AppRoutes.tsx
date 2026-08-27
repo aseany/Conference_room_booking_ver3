86462c1d65fabc87df6cf314874c0d21213b0c7e
tomohisa.kitamura <kitamura@infinity-s.biz>
2026-08-27 19:11:58 +0900

    20260827 tomohisa コメント追加

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,10 +7,15 @@ import SchedulePage from './routes/SchedulePage'
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* URLが /（ルート、つまり親パスと完全一致）のとき、/new へリダイレクトする」というルール */}
       <Route path="/" element={<App />}>
+        {/* URLが /（ルート、つまり親パスと完全一致）のとき、/new へリダイレクトする」というルール */}
         <Route index element={<Navigate to="/new" replace />} />
+        {/* URLが /new のとき、NewBookingPage.tsx コンポーネントを描画する」というルール */}
         <Route path="new" element={<NewBookingPage />} />
+        {/* URLが /schedule のとき、SchedulePage コンポーネントを描画する」というルール */}
         <Route path="schedule" element={<SchedulePage />} />
+        {/* 上記のどのパスにも一致しなかったとき（＝存在しないURL）、/new へリダイレクトする」というルール */}
         <Route path="*" element={<Navigate to="/new" replace />} />
         
       </Route>
